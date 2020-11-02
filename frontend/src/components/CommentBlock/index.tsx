16ca59fdf5abb92f20fa1317c58cb3a4e01751be
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2020-11-02 20:29:55 -0300

    Excluir post e comentarios

--- frontend/src/components/CommentBlock/index.tsx
+++ frontend/src/components/CommentBlock/index.tsx
@@ -3,11 +3,17 @@ import React, { FormEvent, useState } from 'react';
 import './styles.css';
 import CommentInterface from '../../interfaces/CommentInterface';
 import commentIcons from '../../assets/images/icons/message-square.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import getDateTime from '../../helpers/getDateTime';
+import { getToken } from '../../services/auth';
+
+import trashIcon from '../../assets/images/icons/trash.svg';
+
+import loadIcon from '../../assets/images/icons/loader.svg';
 
 
 import getProfileUrl from '../../helpers/getProfileUrl';
+import api from '../../services/api';
 
 interface PostProps {
     comment:CommentInterface
@@ -19,16 +25,36 @@ const CommentBlock:React.FC<PostProps> = (props) => {
     } = props;
 
     const {day,month,hour,minutes} = getDateTime(comment.created_at);
-    
 
+    const location = useLocation();
+    const history = useHistory();
+    const [isInvisible, setIsInvisible] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    
+    async function deleteComment() {
+        setIsLoading(true);
+        try{
+            await api.delete("/comments/"+comment?.id).then((response) => {
+               console.log(response.data);
+               setIsInvisible(true);
+               
+        })} catch(err) {
+            console.log("ERRO AO RECUPERAR COMMENT", err);
+            //setErrorMessage(err?.response?.data?.message);
+        }
+        setIsLoading(false);
+    }
     return (
         
-            <div className="comment-area">
+            <div className={"comment-area "+(isInvisible && "invisible")}>
                 <div className="comment-header">
                     <Link to={`/profile/${comment.user?.id}`}  >
                         <p><img src={(getProfileUrl(comment.user?.id || 0))} alt=""/> {comment.user?.name}</p>
                     </Link>
-                    <p>{hour}:{minutes} {day}/{month}  </p>
+                    <p>
+                        {comment.user?.id?.toString() == getToken() && (<img src={isLoading ? loadIcon :trashIcon } className="trash" onClick={deleteComment} alt=""/>) }
+                        {hour}:{minutes} {day}/{month}  
+                    </p>
 
                 </div>
                 <div className="comment-body">
--- frontend/src/components/PostBlock/index.tsx
+++ frontend/src/components/PostBlock/index.tsx
@@ -3,11 +3,16 @@ import React, { FormEvent, useState } from 'react';
 import './styles.css';
 import PostInterface from '../../interfaces/PostInterface';
 import commentIcons from '../../assets/images/icons/message-square.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import getDateTime from '../../helpers/getDateTime';
+import { getToken } from '../../services/auth';
+
+import trashIcon from '../../assets/images/icons/trash.svg';
+import loadIcon from '../../assets/images/icons/loader.svg';
 
 
 import getProfileUrl from '../../helpers/getProfileUrl';
+import api from '../../services/api';
 
 interface PostProps {
     post:PostInterface
@@ -19,15 +24,39 @@ const PostBlock:React.FC<PostProps> = (props) => {
     } = props;
 
     const {day,month,hour,minutes} = getDateTime(post.created_at);
+    const location = useLocation();
+    const history = useHistory();
+    const [isInvisible, setIsInvisible] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    console.log(location.pathname);
+
+    async function deletePost() {
+        setIsLoading(true);
+        try{
+            await api.delete("/post/"+post?.id).then((response) => {
+               console.log(response.data);
+               setIsInvisible(true);
+               if(location.pathname != "/home" && location.pathname != "/search") {
+                    history.push("/home");
+               }
+        })} catch(err) {
+            console.log("ERRO AO RECUPERAR POST", err);
+            //setErrorMessage(err?.response?.data?.message);
+        }
+        setIsLoading(false);
+    }
 
     return (
         
-            <div className="post-area">
+            <div className={"post-area "+(isInvisible && "invisible")}>
                 <div className="post-header">
                     <Link to={`/profile/${post.user?.id}`}  >
                         <p><img src={(getProfileUrl(post.user?.id || 0))} alt=""/> {post.user?.name}</p>
                     </Link>
-                    <p>{hour}:{minutes} {day}/{month}  </p>
+                    <p> 
+                         {post.user?.id == getToken()?.toString() && (<img src={isLoading ? loadIcon :trashIcon } className="trash" onClick={deletePost} alt=""/>) }
+                        {hour}:{minutes} {day}/{month}  
+                    </p>
 
                 </div>
                 <Link to={`/post/${post.id}`} className="post-body">
